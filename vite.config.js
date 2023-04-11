b83ed9d5faacaf7bbadcee275e67af46032d86cd
mickael <mickael.martel@ciantech.fr>
2023-04-11 23:16:55 +0200

    crypto form,model,dataTable

--- vite.config.js
+++ vite.config.js
@@ -10,16 +10,7 @@ export default defineConfig({
         vue(),
         laravel({
             input: ['resources/js/app.js',
-                'resources/css/bootstrap-extended.css',
-                'resources/css/components.css',
-                'resources/css/login-register.css',
-                'resources/css/material.css',
-                'resources/css/material-colors.css',
-                'resources/css/material-extended.css',
-                'resources/css/material-vendors.min.css',
-                'resources/css/material-vertical-menu-modern.css',
-                'resources/css/photoswipe.css',
-                'resources/css/photoswipe-skin.css',],
+                'resources/css/app.scss',],
             refresh: true,
 
         }),
