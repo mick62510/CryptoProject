7a85d454aa4b103870618204c81a379f4e510cc9
mickael <mickael.martel@ciantech.fr>
2023-04-15 08:26:18 +0200

    correction bootstrap js

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,6 @@ export default defineConfig({
     server: {
         host: '127.0.0.1',
     },
-    resolve: {
-        alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
-    },
     plugins: [
         vue(),
         laravel({
