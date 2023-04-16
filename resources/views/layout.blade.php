d8208ab7e935ddc6c3420300681dfa87c6e3a213
mickael <mickael.martel@ciantech.fr>
2023-04-16 21:42:52 +0200

    Install pwa laravel

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -29,7 +29,6 @@
     <meta name="theme-color" content="#6777ef"/>
     <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
-    @laravelPWA
 </head>
 <!-- END: Head-->
 
