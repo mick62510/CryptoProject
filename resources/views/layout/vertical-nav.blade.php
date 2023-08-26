2871a7cdb55ff776a99cdc47ed790c7149110f27
Mickael <m.mickl@hotmail.fr>
2023-08-26 09:18:42 +0200

    feat google adsense add pub

--- resources/views/layout/vertical-nav.blade.php
+++ resources/views/layout/vertical-nav.blade.php
@@ -36,4 +36,18 @@
             </li>
         </ul>
     </div>
+    <div>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8748011032030103"
+                crossorigin="anonymous"></script>
+        <!-- test -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-8748011032030103"
+             data-ad-slot="8678230168"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
 </div>
