1f4fa6a21a81dd25796721eb02b503febf5f92a0
Mickael <m.mickl@hotmail.fr>
2023-08-26 21:44:09 +0200

    hotfix cookie

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -95,7 +95,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
             integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
             crossorigin="anonymous"></script>
-    <script type="text/javascript" src="https://cookieconsent.popupsmart.com/src/js/popper.js"></script><script> window.start.init({Palette:"palette6",Mode:"floating right",Theme:"wire",Location:"https://traiding-journal.com/cookie-policy",Time:"10",})</script>
+    <script type="text/javascript" src="https://cookieconsent.popupsmart.com/src/js/popper.js"></script><script> window.start.init({Palette:"palette6",Mode:"floating right",Theme:"wire",Location:"https://mon-journal-trading.fr/cookie-policy",Time:"10",})</script>
     @vite('resources/js/btn-confirmation.js')
     @vite('resources/js/image.js')
     @stack('js')
